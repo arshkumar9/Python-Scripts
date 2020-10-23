9542dc1a4c8f4e853d8fe7d3f4de152d7e04e991
Arsh <arshkumar9@live.com>
2020-10-23 16:12:16 +0530

    Just remove an if statement

--- random number game.py
+++ random number game.py
@@ -1,23 +1,25 @@
 import random
-while True:
-
+exit = False
+while exit == False:
     try:
         user_answer=int(input("Guess a number between 1 to 20 : "))
     except ValueError:
         print("Please enter integers only. ")
         continue
-    answer=random.randint(1,2)
+
+    answer=random.randint(1,20)
+
     if answer==user_answer:
         print("Correct! The answer is {}.".format(answer))
     else:
         print("Wrong answer.")
+
     while True:
         response=input("Do you want to continue? (y or n) : ")
         response=response.lower().strip()
+
         if response == 'y' or response == 'yes':
             break
         elif response == 'n' or response == 'no':
             exit=True
             break
-    if exit==True:
-        break
